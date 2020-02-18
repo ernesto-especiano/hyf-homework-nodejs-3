55d0b7c8283c1815307115e57fad95ad85a7fb52
Ernesto Especiano <eespeciano@unitedvirtualities.com>
2020-02-18 13:20:36 -0700

    pushiiing

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ app.get('/users', function(req,res){
     res.send(user);
  })
 
+ app.post('/users',function(req,res){
+     user.push(req.body);
+     user = [
+         {id:0}
+     ]
+ })
+
+ app.get('/users/:id',function(req,res){
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message:'got one user',
+        user: req.params.id
+    })
+})
+
 
 app.listen(3000,function(){
     console.log("server is running")
